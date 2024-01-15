6507b1608943c570ecc99db97e78ef9c571ff5e1
lanzuln <lanzu.dev@gmail.com>
2024-01-16 00:03:05 +0600

    product mode, migration, factory done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Counter;
+use App\Models\product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-       Counter::factory(1)->create();
+        // Counter::factory(1)->create();
+        product::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
